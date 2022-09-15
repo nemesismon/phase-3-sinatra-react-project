c827654d4a72601506e91df6b82fbf557afd2840
Michael <michael.w.kolb@gmail.com>
2022-09-15 12:01:46 -0400

    Added seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,8 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+Guess.create(actor: "Michael Keaton", player_id: 1)
+Guess.create(actor: "Meryl Streep", player_id: 2)
+Guess.create(actor: "Brad Pitt", player_id: 3)
+
 puts "✅ Done seeding!"
