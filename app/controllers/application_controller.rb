7e3708daa326c0c305ff43f3a12026e928de0813
Michael <michael.w.kolb@gmail.com>
2022-09-22 11:01:05 -0400

    Project meeting with Nancy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,53 +1,50 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
   get "/" do
     { message: "The Name Game!" }.to_json
   end
 
   #players routing
   get '/players' do
-    players = Player.all
-    players.to_json(include :guesses)
+    Player.all.to_json
   end
 
   post '/players' do
-    player = Player.create(name: params[:name])
-    player.to_json
+    Player.create(name: params[:name])
+    Player.all.to_json
   end
 
   patch '/players/:id' do
     player = Player.find(id: params[:id])
     player.update(name: params[:name])
-    player.to_json
+    Player.all.to_json
   end
 
 
   #guesses routing
 
   get '/guesses' do
-    guesses = Guess.all
-    guesses.to_json(include :players)
+    Guess.all.to_json
   end
 
   post '/guesses' do
-    guess = Guess.create(
+    Guess.create(
       actor: params[:actor],
-      player_id: params[:player_id]
     )
-    guess.to_json
+    Guess.all.to_json
   end
 
   patch '/guesses/:id' do
-    guess = Guess.find(params[:id])
+    guess = Guess.find_by(params[:id])
     guess.update(actor: params[:actor])
-    guess.to_json
+    Guess.all.to_json
   end
 
   delete '/guesses/:id' do
     guess = Guess.find(params[:id])
-    guess.destory
+    guess.destroy
+    Guess.all.to_json
   end
 
 end
